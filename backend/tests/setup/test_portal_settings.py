f44bf5e1541dfec05a2be498cc526ce63fba9b03
Humanaice <matheusnsantana@gmail.com>
2025-08-15 09:12:24 -0300

    Exercício 17: Arrumado - Testes de configuração do backend

--- backend/tests/setup/test_portal_settings.py
+++ backend/tests/setup/test_portal_settings.py
@@ -1,11 +1,24 @@
+"""Portal settings tests."""
+
 from plone import api
+
+import pytest
 
 
 class TestPortalSettings:
     """Test that Portal configuration is correctly done."""
 
-    def test_portal_title(self, portal):
-        """Test portal title."""
-        value = api.portal.get_registry_record("plone.site_title")
-        expected = "Intranet do TRE-DF"
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ["plone.site_title", "Intranet do TRE-DF"],
+            ["plone.email_from_name", "e-Intranet TRE-DF"],
+            ["plone.smtp_host", "localhost"],
+            ["plone.smtp_port", 25],
+            ["plone.navigation_depth", 4],
+        ],
+    )
+    def test_setting(self, portal, key: str, expected: str | int):
+        """Test registry setting."""
+        value = api.portal.get_registry_record(key)
         assert value == expected
