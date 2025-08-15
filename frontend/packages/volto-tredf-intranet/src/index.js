972ac8f6b081b744381bdae430e616dfcb4ac113
Humanaice <matheusnsantana@gmail.com>
2025-08-15 14:30:32 -0300

    Exercício 23: Visão para o tipo Área

--- frontend/packages/volto-tredf-intranet/src/index.js
+++ frontend/packages/volto-tredf-intranet/src/index.js
@@ -1,23 +1,28 @@
+import config from '@plone/volto/registry';
+import AreaView from './components/Views/AreaView';
+
 const applyConfig = (config) => {
-  config.settings = {
-    ...config.settings,
-    isMultilingual: false,
-    supportedLanguages: ['pt-br'],
-    defaultLanguage: 'pt-br',
-    image_crop_aspect_ratios: [
-      {
-        label: '16:9',
-        ratio: 16 / 9,
-      },
-      {
-        label: '4:3',
-        ratio: 4 / 3,
-      },
-      {
-        label: '1:1',
-        ratio: 1,
-      },
-    ],
+  config.settings.isMultilingual = false;
+  config.settings.supportedLanguages = ['pt-br'];
+  config.settings.defaultLanguage = 'pt-br';
+  config.settings.image_crop_aspect_ratios = [
+    {
+      label: '16:9',
+      ratio: 16 / 9,
+    },
+    {
+      label: '4:3',
+      ratio: 4 / 3,
+    },
+    {
+      label: '1:1',
+      ratio: 1,
+    },
+  ];
+
+  config.views.contentTypesViews = {
+    ...config.views.contentTypesViews,
+    Area: AreaView,
   };
   return config;
 };
